fdcbffa25a135300fc4fad54026df2661cf20956
arnavsurve <arnavsurve@gmail.com>
2018-07-08 11:51:45 -0700

    Added root function

--- calculator.py
+++ calculator.py
@@ -15,14 +15,18 @@ def divide(x, y):
 def exponent(x, y):
     return x ** y
 
+def root(x, y):
+    return x ** (1/y)
+
 print ("Select operation:")
 print ("1. Addition")
 print ("2. Subtraction")
 print ("3. Multiplication")
 print ("4. Division")
 print ("5. Exponent")
+print ("6. Root")
 
-choice = input("Enter choice: (1/2/3/4/5):")
+choice = input("Enter choice: (1/2/3/4/5/6):")
 
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
@@ -37,6 +41,8 @@ elif choice == '4':
     print(num1,"/",num2,"=", divide(num1,num2))
 elif choice == "5":
     print(num1,"**",num2,"=", exponent(num1,num2))
+elif choice == "6":
+    print(num2,"\/`",num1,"=", root(num1,num2))
 else:
    print("Invalid input")
 
