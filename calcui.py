dc8f8e8535f7338837df8c06c336e5a20e08561c
arnavsurve <arnavsurve@gmail.com>
2018-07-11 17:13:49 -0700

    implemented exit button

--- calcui.py
+++ calcui.py
@@ -1,9 +1,22 @@
 from tkinter import *
 
+class calculator:
+    
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+
+        self.exitButton = Button(frame, text="OFF", command=self.exitProgram)
+        self.exitButton.grid(row=0, column=0, sticky=W)
+
+    def exitProgram(self):
+        raise SystemExit
 
 
 root = Tk()
 
+
+b = calculator(root)
 
 
 root.mainloop()
