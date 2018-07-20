8155f594283e6176657c5a72816699ec6c0abb8b
arnavsurve <arnavsurve@gmail.com>
2018-07-19 22:23:58 -0700

    added an output label (still needs lots of work)

--- calcui.py
+++ calcui.py
@@ -24,55 +24,79 @@ class arnavButtons:
         self.secondEntry = Entry(frame, bg="#34435b", highlightbackground="#021635", fg="white")
         self.secondEntry.grid(row=2, column=1)
 
-#        self.outputLabel = Label(frame, text=(int(self.firstEntry.get()) + int(self.secondEntry.get())))
+#        self.outputLabel = Label(frame, text=(self.firstEntry.get() + "+" + int(self.secondEntry.get() + "=" + str(int(self.firstEntry.get()) + int(self.secondEntry.get())))
 #        self.outputLabel.grid(row=2, column=3)
 
 
         # Buttons for math functions along with the functions
         
         def add():
-             print(int(self.firstEntry.get()) + int(self.secondEntry.get()))
+            result = (self.firstEntry.get() + "+" + self.secondEntry.get() + "=" + (str(int(self.firstEntry.get()) + int(self.secondEntry.get()))))
+
+            self.outputBox = Label(frame, text=result, bg="#021635", fg="white", borderwidth="2", width="20")
+            self.outputBox.grid(row=5, column=0)
 
         self.addButton = Button(frame, text=" + ", highlightbackground="#021635", fg="white", command=add)
         self.addButton.grid(row=0, column=2, pady=3, padx=3)
         
         def subtract():
-             print(int(self.firstEntry.get()) - int(self.secondEntry.get()))
+            result = (self.firstEntry.get() + "-" + self.secondEntry.get() + "=" + str(int(self.firstEntry.get()) - int(self.secondEntry.get())))
         
+            self.outputBox = Label(frame, text=result, bg="#021635", fg="white", borderwidth="2", width="20")
+            self.outputBox.grid(row=5, column=0)
+
         self.subtractButton = Button(frame, text=" - ", highlightbackground="#021635", fg="white", command=subtract)
         self.subtractButton.grid(row=1, column=2, pady=3, padx=10)
 
         def multiply():
-             print(int(self.firstEntry.get()) * int(self.secondEntry.get()))
+            result = (self.firstEntry.get() + "*" + self.secondEntry.get() + "=" + str(int(self.firstEntry.get()) * int(self.secondEntry.get())))
         
+            self.outputBox = Label(frame, text=result, bg="#021635", fg="white", borderwidth="2", width="20")
+            self.outputBox.grid(row=5, column=0)
+
         self.multiplyButton = Button(frame, text=" * ", highlightbackground="#021635", fg="white", command=multiply)
         self.multiplyButton.grid(row=2, column=2, pady=3, padx=10)
 
         def divide():
-            print(int(self.firstEntry.get()) / int(self.secondEntry.get()))
-             
+            result = (self.firstEntry.get() + "/" + self.secondEntry.get() + "=" + str(int(self.firstEntry.get()) / int(self.secondEntry.get())))
+
+            self.outputBox = Label(frame, text=result, bg="#021635", fg="white", borderwidth="2", width="20")
+            self.outputBox.grid(row=5, column=0)
+
         self.divideButton = Button(frame, text=" / ", highlightbackground="#021635", fg="white", command=divide)
         self.divideButton.grid(row=3, column=2, pady=3, padx=10)
 
         def exponent():
-             print(int(self.firstEntry.get()) ** int(self.secondEntry.get()))
+            result = (self.firstEntry.get() + "^" + self.secondEntry.get() + "=" + str(int(self.firstEntry.get()) ** int(self.secondEntry.get())))
         
+            self.outputBox = Label(frame, text=result, bg="#021635", fg="white", borderwidth="2", width="20")
+            self.outputBox.grid(row=5, column=0)
+
         self.exponentButton = Button(frame, text=" ^ ", highlightbackground="#021635", fg="white", command=exponent)
         self.exponentButton.grid(row=4, column=2, pady=3, padx=10)
 
         def root():
-             print((int(self.firstEntry.get())) ** (1/(int(self.secondEntry.get()))))
+            result = ((self.firstEntry.get() + "root" + self.secondEntry.get() + "=" + (str(int(self.firstEntry.get())) ** (1/(int(self.secondEntry.get()))))))
         
+            self.outputBox = Label(frame, text=result, bg="#021635", fg="white", borderwidth="2", width="20")
+            self.outputBox.grid(row=5, column=0)
+
         self.rootButton = Button(frame, text=" root ", highlightbackground="#021635", fg="white", command=root)
         self.rootButton.grid(row=5, column=2, pady=3, padx=3)
 
         def factorial():
-             from math import factorial
-             print(factorial(int(self.firstEntry.get())))
+            from math import factorial
+            result = (factorial(int((self.firstEntry.get()))) + "! " + "=" + (str(int(self.firstEntry.get()))))
         
+            self.outputBox = Label(frame, text=result, bg="#021635", fg="white", borderwidth="2", width="20")
+            self.outputBox.grid(row=5, column=0)
+
         self.factorialButton = Button(frame, text=" ! ", highlightbackground="#021635", fg="white", command=factorial)
         self.factorialButton.grid(row=6, column=2, pady=3, padx=10)
 
+#        adding an output box
+#        self.outputBox = Label(frame, text=result.get(), bg="#021635", fg="white")
+#        self.outputBox.grid(row=5, column=1)
 
 root = Tk()
 
